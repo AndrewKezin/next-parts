3806cd69a8d454c296f0f2f33b0d424c55b565ee
AndrewKezin <122410859+AndrewKezin@users.noreply.github.com>
2025-08-17 18:00:04 +0300

    fix checkout bug with addresses

--- components/checkout/checkout-addresses-block.tsx
+++ components/checkout/checkout-addresses-block.tsx
@@ -29,8 +29,8 @@ export const CheckoutAddressesBlock: React.FC<Props> = ({ addresses, className }
 
   const { control } = useFormContext();
 
-  if (addresses.length === 0)
-    return <h3 className="text-xl font-bold mb-5 text-center">Нет сохраненных адресов</h3>;
+  // if (addresses.length === 0)
+  //   return <h3 className="text-xl font-bold mb-5 text-center">Нет сохраненных адресов</h3>;
 
   const handleChangeAddress = (id: number, onChange: (addressId: number | null) => void) => {
     if (currentAddress === id) {
@@ -58,38 +58,40 @@ export const CheckoutAddressesBlock: React.FC<Props> = ({ addresses, className }
 
   return (
     <>
-      <Controller
-        name="address"
-        control={control}
-        render={({ field }) => (
-          <div className="w-full flex flex-col gap-3 mb-5">
-            {addresses.map((address) => (
-              <div
-                key={address.id}
-                className={cn(
-                  'w-full flex gap-5 rounded-md bg-gray-200 py-3 md:py-5 px-3 hover:text-primary hover:border-primary cursor-pointer transition ease-in-out',
-                  { 'border-2 border-primary bg-white': currentAddress === address.id },
-                )}
-                onClick={() => handleChangeAddress(address.id, field.onChange)}>
-                <div className="w-full flex justify-between items-center text-md">
-                  {address.address}
-                  {currentAddress === address.id && (
-                    <div className="text-green-500 text-2xl p-1">✅</div>
+      {addresses.length === 0 && <h3 className="text-xl font-bold mb-5 text-center">Нет сохраненных адресов</h3>}
+      {addresses.length > 0 && (
+        <Controller
+          name="address"
+          control={control}
+          render={({ field }) => (
+            <div className="w-full flex flex-col gap-3 mb-5">
+              {addresses.map((address) => (
+                <div
+                  key={address.id}
+                  className={cn(
+                    'w-full flex gap-5 rounded-md bg-gray-200 py-3 md:py-5 px-3 hover:text-primary hover:border-primary cursor-pointer transition ease-in-out',
+                    { 'border-2 border-primary bg-white': currentAddress === address.id },
                   )}
+                  onClick={() => handleChangeAddress(address.id, field.onChange)}>
+                  <div className="w-full flex justify-between items-center text-md">
+                    {address.address}
+                    {currentAddress === address.id && (
+                      <div className="text-green-500 text-2xl p-1">✅</div>
+                    )}
 
-                  {/* Удаление адеса */}
-                  <div
-                    className="w-[30px] h-[30px] flex justify-center items-center border-2 border-gray-500 rounded-[5px] text-gray-500 cursor-pointer hover:border-primary"
-                    onClick={(e) => onClickAddressDelete(e, address.id)}>
-                    <Trash size={20} />
+                    {/* Удаление адеса */}
+                    <div
+                      className="w-[30px] h-[30px] flex justify-center items-center border-2 border-gray-500 rounded-[5px] text-gray-500 cursor-pointer hover:border-primary"
+                      onClick={(e) => onClickAddressDelete(e, address.id)}>
+                      <Trash size={20} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
-      />
-
+              ))}
+            </div>
+          )}
+        />
+      )}
       <h3 className="text-md font-bold">Добавить новый адрес доставки: </h3>
       <AddressInput onChange={(value) => setNewAddress(value)} />
       <div
